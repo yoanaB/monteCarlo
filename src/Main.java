4a74a53270ec22fc4c3081fcd6902a8c760d3f7e
Yoana Boyanova <yoana.boyanova95@gmail.com>
2016-06-12 16:17:26 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+
 /**
  * Created by Yoana on 12.6.2016 г..
  */
 public class Main {
     public static void main(String[] Args){
 
-        MonteCarloRunner runner = new MonteCarloRunner(20000000, 40);
+       // MonteCarloRunner runner = new MonteCarloRunner(20000000, 40);
+        MonteCarloRunner runner = new MonteCarloRunner(new Integer(Args[0]), new Integer(Args[1]));
         runner.run();
     }
 }
